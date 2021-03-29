2e072069e97899c19638a0314ca7ab98d0db4131
Valentín Nicheglod <nicheglod69@gmail.com>
2021-03-29 18:45:33 -0300

    fix

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 import App from './App';
 import store from './Redux/Index';
